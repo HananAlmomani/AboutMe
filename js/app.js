5d2d219b1b982fc38c331132dbcc7073892a7908
HananAlmomani <hananalmomani11@yahoo.com>
2021-03-16 13:33:17 +0200

    added files

--- js/app.js
+++ js/app.js
@@ -7,19 +7,34 @@ alert('Hi there ' + variable2 );
 
 alert('lets see if you will know me or not,ready? set ,gooo!');
 
-let userName =prompt('Is my name Hanan?');
+alert('please answer with yes or no');
 
-if (userName.toLowerCase() === 'no')
+let userName =prompt('Is my name Hanan?');
+while((userName !== 'yes' && userName !== 'y') && (userName !== 'no' && userName !== 'n'))
+{
+  userName =prompt('Is my name Hanan?');
+
+}
+if (userName.toLowerCase() === 'no'|| userName.toLowerCase() === 'n')
 {
   alert('your answer is wrong, im sad!');
   console.log(userName);
-} else {
+}
+else if(userName.toLowerCase() === 'yes')
+{
   alert('your answer is so correct, happy me!!');
   console.log(userName);
 }
 
+
+
 let age =prompt('Do you think im older than 40?');
-if (age.toLowerCase() === 'no')
+while((age !== 'yes' && age !== 'y') && (age !== 'no' && age !== 'n'))
+{
+  age =prompt('Do you think im older than 40?');
+
+}
+if (age.toLowerCase() === 'no'|| age.toLowerCase() === 'n')
 {
   alert('yes yes good answer');
   console.log(age);
@@ -29,7 +44,13 @@ if (age.toLowerCase() === 'no')
 }
 
 let country=prompt('Do i live in maldives?');
-if (country.toLowerCase() === 'no') {
+while((country !== 'yes' && country !== 'y') && (country !== 'no' && country !== 'n'))
+{
+  country =prompt('Do i live in maldives?');
+
+}
+if (country.toLowerCase() === 'no'|| country.toLowerCase() === 'n')
+{
   alert('yes im poor');
   console.log(country);
 
@@ -39,7 +60,12 @@ if (country.toLowerCase() === 'no') {
 }
 
 let myMajor =prompt('Am i majored in physics?');
-if (myMajor.toLowerCase() === 'no')
+while((myMajor !== 'yes' && myMajor !== 'y') && (myMajor !== 'no' && myMajor!== 'n'))
+{
+  myMajor =prompt('Am i majored in physics?');
+
+}
+if (myMajor.toLowerCase() === 'no'|| myMajor.toLowerCase() === 'n')
 {
   alert('you got that right!');
 } else {
@@ -48,7 +74,12 @@ if (myMajor.toLowerCase() === 'no')
 
 
 let hobby =prompt('do you think i like playing chess?');
-if (hobby.toLowerCase() === 'no')
+while((hobby !== 'yes' && hobby!== 'y') && (hobby !== 'no' && hobby !== 'n'))
+{
+  hobby =prompt('do you think i like playing chess?');
+
+}
+if (hobby.toLowerCase() === 'no' || hobby.toLowerCase() === 'n')
 {
   alert('actually i do');
 } else
