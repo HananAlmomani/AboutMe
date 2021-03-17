1a7c4eccae228d666c9417dbceb6fbffec61daf2
HananAlmomani <hananalmomani11@yahoo.com>
2021-03-17 18:05:14 +0200

    added edits

--- js/app.js
+++ js/app.js
@@ -15,158 +15,164 @@ alert('please answer with yes or no');
 let myName = function()
 {
   let userName =prompt('Is my name Hanan?');
-while((userName !== 'yes' && userName !== 'y') && (userName !== 'no' && userName !== 'n'))
-{
-  userName =prompt('Is my name Hanan?');
+  while((userName !== 'yes' && userName !== 'y') && (userName !== 'no' && userName !== 'n'))
+  {
+    userName =prompt('Is my name Hanan?');
 
-}
-if (userName.toLowerCase() === 'no'|| userName.toLowerCase() === 'n')
-{
-  alert('your answer is wrong, im sad!');
-  console.log(userName);
-}
-else if(userName.toLowerCase() === 'yes')
-{
-  alert('your answer is so correct, happy me!!');
-  console.log(userName);
-  score++;
-}
-}
- myName();
+  }
+  if (userName.toLowerCase() === 'no'|| userName.toLowerCase() === 'n')
+  {
+    alert('your answer is wrong, im sad!');
+    console.log(userName);
+  }
+  else if(userName.toLowerCase() === 'yes' || userName.toLocaleLowerCase()=== 'y')
+  {
+    alert('your answer is so correct, happy me!!');
+    console.log(userName);
+    score++;
+  }
+};
+myName();
 
 
 let myAge = function()
 {
   let age =prompt('Do you think im older than 40?');
-while((age !== 'yes' && age !== 'y') && (age !== 'no' && age !== 'n'))
-{
-  age =prompt('Do you think im older than 40?');
+  while((age !== 'yes' && age !== 'y') && (age !== 'no' && age !== 'n'))
+  {
+    age =prompt('Do you think im older than 40?');
 
-}
-if (age.toLowerCase() === 'no'|| age.toLowerCase() === 'n')
-{
-  alert('yes yes good answer');
-  console.log(age);
-  score++;
-} else {
-  alert('now im mad im not that old');
-  console.log(age);
-}
-}
+  }
+  if (age.toLowerCase() === 'no'|| age.toLowerCase() === 'n')
+  {
+    alert('yes yes good answer');
+    console.log(age);
+    score++;
+  } else {
+    alert('now im mad im not that old');
+    console.log(age);
+  }
+};
 myAge();
 
 let myCountry = function()
 {
   let country=prompt('Do i live in maldives?');
-while((country !== 'yes' && country !== 'y') && (country !== 'no' && country !== 'n'))
-{
-  country =prompt('Do i live in maldives?');
+  while((country !== 'yes' && country !== 'y') && (country !== 'no' && country !== 'n'))
+  {
+    country =prompt('Do i live in maldives?');
 
-}
-if (country.toLowerCase() === 'no'|| country.toLowerCase() === 'n')
-{
-  alert('yes im poor');
-  console.log(country);
-  score++;
+  }
+  if (country.toLowerCase() === 'no'|| country.toLowerCase() === 'n')
+  {
+    alert('yes im poor');
+    console.log(country);
+    score++;
 
-} else {
-  alert('i really wish that!');
-  console.log(country);
-}
-}
-myCountry(); 
+  } else {
+    alert('i really wish that!');
+    console.log(country);
+  }
+};
+myCountry();
 
 let major = function()
 {
   let myMajor =prompt('Am i majored in physics?');
-while((myMajor !== 'yes' && myMajor !== 'y') && (myMajor !== 'no' && myMajor!== 'n'))
-{
-  myMajor =prompt('Am i majored in physics?');
+  while((myMajor !== 'yes' && myMajor !== 'y') && (myMajor !== 'no' && myMajor!== 'n'))
+  {
+    myMajor =prompt('Am i majored in physics?');
 
-}
-if (myMajor.toLowerCase() === 'no'|| myMajor.toLowerCase() === 'n')
-{
-  alert('you got that right!');
-  console.log(myMajor);
-  score++;
-} else {
-  alert('no i hate physics');
-  console.log(myMajor);
-}
-}
-major(); 
+  }
+  if (myMajor.toLowerCase() === 'no'|| myMajor.toLowerCase() === 'n')
+  {
+    alert('you got that right!');
+    console.log(myMajor);
+    score++;
+  } else {
+    alert('no i hate physics');
+    console.log(myMajor);
+  }
+};
+major();
 
 
 let hobby = function()
 {
   let hobby =prompt('do you think i like playing chess?');
-while((hobby !== 'yes' && hobby!== 'y') && (hobby !== 'no' && hobby !== 'n'))
-{
-  hobby =prompt('do you think i like playing chess?');
+  while((hobby !== 'yes' && hobby!== 'y') && (hobby !== 'no' && hobby !== 'n'))
+  {
+    hobby =prompt('do you think i like playing chess?');
 
-}
-if (hobby.toLowerCase() === 'no' || hobby.toLowerCase() === 'n')
-{
-  alert('actually i do');
-  console.log(hobby);
+  }
+  if (hobby.toLowerCase() === 'no' || hobby.toLowerCase() === 'n')
+  {
+    alert('actually i do');
+    console.log(hobby);
 
-} else
-{
-  alert('so true!');
-  console.log(hobby);
-  score++;
-}
-}
-hobby(); 
+  } else
+  {
+    alert('so true!');
+    console.log(hobby);
+    score++;
+  }
+};
+hobby();
 
 let q6=function ()
 {
   let number = prompt('Now guess how many movies i love between 5-15?');
-for (let i=4 ; i>1 ; i--)
-{
-  if (number === '10')
+  for (let i=4 ; i>1 ; i--)
   {
-    alert('You are great');
-    console.log(number);
-    score++;
-    break;
+    if (number === '10')
+    {
+      alert('You are great');
+      console.log(number);
+      score++;
+      break;
+    }
+    else if(number>10)
+    {
+      alert('You are too high');
+      console.log(number);
+      number = prompt('No guess the number between 5-15 another time ? you have just  ' +i+' time');
+    }
+    else if(number<10)
+    {
+      alert('You are too low');
+      console.log(number);
+      number = prompt('No guess the number between 5-15 another time ? you have just' +i+' time');
+    }
   }
-  else if(number>10)
-  {
-    alert('You are too high');
-    console.log(number);
-    number = prompt('No guess the number between 5-15 another time ? you have just  ' +i+' time');
-  }
-  else if(number<10)
-  {
-    alert('You are too low');
-    console.log(number);
-    number = prompt('No guess the number between 5-15 another time ? you have just' +i+' time');
-  }
-}
-}
+};
 
 q6();
 let q7 = function()
 {
   let marr=[7,10,3];
-alert('can you guess my 3 favorite numbers?');
-let fav = prompt('guess one number of them between 2-20 (you have 6 attemp)');
-for (let h=5 ;h>0 ; h--)
-{
-  if (fav == marr[0] || fav == marr[1] || fav ==marr[2] )
+  alert('can you guess my 3 favorite numbers?');
+  let fav =Number(prompt('guess one number of them between 2-20 (you have 6 attemp)'));
+
+
+  for (let h=5 ;h>0 ; h--)
   {
-    alert('you are great');
-    console.log(fav);
-    score++;
-    h=0;
+    for (let i=0;i<3;i++)
+    {
+      if (fav === marr[i])
+      {
+        alert('you are great');
+        console.log(fav);
+        score++;
+        h=0;
+      }
+    }
+    if(h!==0){
+      fav = prompt('guess one number of them between 2-20 you have just ' +h+ ' attemp');
+    }
+
   }
-  else
-  {
-    fav = prompt('guess one number of them between 2-20 you have just ' +h+ ' attemp');
-  }
-}
-}
+  alert('the correct answers were 3,7,10');
+};
 
 q7();
 alert('you got  '+score+' out of 7') ;
